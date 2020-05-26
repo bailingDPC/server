036cb8a211b52b20d86f037982c0c7956c31a3b3
bailingDPC <50673418+bailingDPC@users.noreply.github.com>
2020-05-26 10:30:33 +0800

    添加了静态资源目录view

--- app.js
+++ app.js
@@ -22,8 +22,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, '../client/dist')));
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'view')));
 
 //允许跨域
 app.use(function (req, res, next) {
